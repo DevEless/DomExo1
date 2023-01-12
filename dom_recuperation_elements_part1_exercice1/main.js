a138f340af4b31b4668db166afadb4f6b00877b5
Quent_DLM <qdelameilleure@gmail.com>
2023-01-12 14:46:19 +0100

    fin

---
+++ dom_recuperation_elements_part1_exercice1/main.js
@@ -0,0 +1,18 @@
+let titreH1 = document.getElementsByTagName("h1")
+console.log(titreH1)
+
+let paragraphe = document.getElementsByTagName("h3");
+let para1 = h3[0];
+let para2 = h3[1];
+let para3 = h3[2];
+console.log(para1);
+console.log(para2);
+console.log(para3);
+
+let text = document.getElementsByTagName("p");
+let text2 = p[1];
+console.log(text2);
+
+let col = document.getElementsByTagName("li");
+let col3 = li[2];
+console.log(col3);
